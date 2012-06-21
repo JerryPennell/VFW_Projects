45218e4af13efc6e2c58af0105232cee443d58f5
Jerry Pennell <jwpenn@fullsail.edu>
2012-06-20 22:44:09 -0400

    Adding flow chart for Project 4 and CSS changes
    Added more colorful toolbar

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@
 //Wait until the DOM is ready
 window.addEventListener("DOMContentLoaded", function(){
 	
-
      //getElementById function
      function $(x){
            var theElement = document.getElementById(x);          //Pass in name for getting tag name by Id
@@ -172,8 +171,9 @@ window.addEventListener("DOMContentLoaded", function(){
 		  var editLink = document.createElement('a');							//Creates anchor tag
 		  editLink.href = "#";													//sets the value to pound tag
 		  editLink.key = key;													//gets the key for the item being edited
-		  var editText = "Edit Comic";									    	//Creates text of Edit comic
+		  var editText = "Edit Comic";									    	//Creates text of Edit comic	
 		  editLink.addEventListener("click", editItem);							//Adds the event listener for the edit item link
+		  editLink.setAttribute("class","sublinks");                            //Adds class sublinks for css
 		  editLink.innerHTML = editText;										//adds the link to the html
 		  linksLi.appendChild(editLink);										//by appending the edit link
 		  
@@ -187,6 +187,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		  deleteLink.key = key;													//sets the key to the comic being deleted
 		  var deleteText = "Delete Comic";										//creates the text to delete the comic
 		  deleteLink.addEventListener("click", deleteItem);						//adds the event listener to delete the item
+		  deleteLink.setAttribute("class","sublinks");
 		  deleteLink.innerHTML = deleteText;									//adds the link to the html
 		  linksLi.appendChild(deleteLink);										//appends the deletelink child 
 	  }
